1a6a06bdeb5ff37c3cef824df0f5749f1d82414f
Noam Shabtai <shabtai.noam@gmail.com>
2026-02-22 10:41:04 +0200

    refactor epoch and parameters.

--- model/src/model/model.py
+++ model/src/model/model.py
@@ -1,12 +1,15 @@
 import torch
-from torch import nn
 
 from . import network
 
 
 class Model:
     def __init__(self, **kwargs):
-        self.model = network.Network(**kwargs)
+        self.epochs = kwargs.get("epochs")
+        self.learning_rate = kwargs.get("learning_rate")
+        self.batch_size = kwargs.get("batch_size")
+        self.data_shuffle = kwargs.get("data_shuffle", True)
+        self.model = network.Network(**kwargs["network"])
 
     def execute(self, features):
         self.model.eval()
@@ -15,26 +18,28 @@ class Model:
             output = self.model(x)
             return output.squeeze(0).numpy()
 
-    def backward(self, data, targets, epochs, lr, batch_size=None):
+    def backward(self, data, targets):
         self.model.train()
-        optimizer = torch.optim.Adam(self.model.parameters(), lr=lr)
-        criterion = nn.MSELoss()
+        optimizer = torch.optim.Adam(self.model.parameters(), lr=self.learning_rate)
+        criterion = torch.nn.MSELoss()
         x = torch.from_numpy(data)
         y = torch.from_numpy(targets)
-        if batch_size is None:
-            batch_size = len(x)
-        epoch_loss = None
-        for _ in range(epochs):
+        batch_size = self.batch_size if self.batch_size is not None else len(x)
+        loader = torch.utils.data.DataLoader(
+            torch.utils.data.TensorDataset(x, y), batch_size=batch_size, shuffle=self.data_shuffle
+        )
+        epoch_losses = []
+        for _ in range(self.epochs):
             batch_losses = []
-            for x_batch, y_batch in zip(x.split(batch_size), y.split(batch_size)):
+            for x_batch, y_batch in loader:
                 optimizer.zero_grad()
                 output = self.model(x_batch)
                 loss = criterion(output, y_batch)
                 loss.backward()
                 optimizer.step()
                 batch_losses.append(loss.item())
-            epoch_loss = sum(batch_losses) / len(batch_losses)
-        return epoch_loss
+            epoch_losses.append(sum(batch_losses) / len(batch_losses))
+        return epoch_losses
 
     def save(self, path):
         torch.save(self.model.state_dict(), path)
